bd8d724ccf25817e638422c47a1a906b9ddb08f3
saurabh.dudhalwar <saurabh.dudhalwar@riomed.com>
2023-06-25 20:52:59 +0530

    Common

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Cellma Component
+export { default as CellmaAutocompleteField } from "./CellmaComponent/CellmaAutocompleteField";
+export { default as CellmaAutoSelectField } from "./CellmaComponent/CellmaAutoSelectField";
+export { default as CellmaButton } from "./CellmaComponent/CellmaButton";
+export { default as CellmaCalendarPicker } from "./CellmaComponent/CellmaCalendarPicker";
+export { default as CellmaCard } from "./CellmaComponent/CellmaCard";
+export { default as CellmaCheckbox } from "./CellmaComponent/CellmaCheckbox";
+export { default as CellmaDatePicker } from "./CellmaComponent/CellmaDatePicker";
+export { default as CellmaDistrictSearch } from "./CellmaComponent/CellmaDistrictSearch";//custom props - useDistrictSearch, translate
+export { default as CellmaDrawer } from "./CellmaComponent/CellmaDrawer";
+export { default as CellmaFooter } from "./CellmaComponent/CellmaFooter";//custom props - translate
+export { default as CellmaInputField } from "./CellmaComponent/CellmaInputField";
+export { default as CellmaLink } from "./CellmaComponent/CellmaLink";
+export { default as CellmaNoDataFound } from "./CellmaComponent/CellmaNoDataFound";//custom props - translate
+export { default as CellmaPagination } from "./CellmaComponent/CellmaPagination";//custom props - dispatch, setPaginationNumber
+export { default as CellmaPopup } from "./CellmaComponent/CellmaPopup";
+export { default as CellmaPostCodeSearch } from "./CellmaComponent/CellmaPostCodeSearch";// custom props - translate
+export { default as CellmaRadioButtonGroup } from "./CellmaComponent/CellmaRadioButtonGroup";
+export { default as CellmaSelectField } from "./CellmaComponent/CellmaSelectField";
+export { default as CellmaSelectFieldWithCheckbox } from "./CellmaComponent/CellmaSelectFieldWithCheckbox"; //custom props - translate
+export { default as CellmaSnackbar } from "./CellmaComponent/CellmaSnackbar";//custom props - dispatch, useSelector, setSnackbar
+export { default as CellmaStepper } from "./CellmaComponent/CellmaStepper";
+export { default as CellmaTimePicker } from "./CellmaComponent/CellmaTimePicker";
+export { default as CellmaUnderConstruction } from "./CellmaComponent/CellmaUnderConstruction";//custom props - isUnderConstruction, navigate, dispatch, translate, setIsUnderConstruction
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Cellma Component
+export { default as CellmaAutocompleteField } from "./CellmaComponent/CellmaAutocompleteField";
+export { default as CellmaAutoSelectField } from "./CellmaComponent/CellmaAutoSelectField";
+export { default as CellmaButton } from "./CellmaComponent/CellmaButton";
+// export { default as CellmaCalendarPicker } from "./CellmaComponent/CellmaCalendarPicker";
+// export { default as CellmaCard } from "./CellmaComponent/CellmaCard";
+// export { default as CellmaCheckbox } from "./CellmaComponent/CellmaCheckbox";
+// export { default as CellmaDatePicker } from "./CellmaComponent/CellmaDatePicker";
+// export { default as CellmaDistrictSearch } from "./CellmaComponent/CellmaDistrictSearch";//custom props - useDistrictSearch, translate
+// export { default as CellmaDrawer } from "./CellmaComponent/CellmaDrawer";
+// export { default as CellmaFooter } from "./CellmaComponent/CellmaFooter";//custom props - translate
+export { default as CellmaInputField } from "./CellmaComponent/CellmaInputField";
+// export { default as CellmaLink } from "./CellmaComponent/CellmaLink";
+// export { default as CellmaNoDataFound } from "./CellmaComponent/CellmaNoDataFound";//custom props - translate
+// export { default as CellmaPagination } from "./CellmaComponent/CellmaPagination";//custom props - dispatch, setPaginationNumber
+// export { default as CellmaPopup } from "./CellmaComponent/CellmaPopup";
+// export { default as CellmaPostCodeSearch } from "./CellmaComponent/CellmaPostCodeSearch";// custom props - translate
+// export { default as CellmaRadioButtonGroup } from "./CellmaComponent/CellmaRadioButtonGroup";
+// export { default as CellmaSelectField } from "./CellmaComponent/CellmaSelectField";
+// export { default as CellmaSelectFieldWithCheckbox } from "./CellmaComponent/CellmaSelectFieldWithCheckbox"; //custom props - translate
+// export { default as CellmaSnackbar } from "./CellmaComponent/CellmaSnackbar";//custom props - dispatch, useSelector, setSnackbar
+// export { default as CellmaStepper } from "./CellmaComponent/CellmaStepper";
+// export { default as CellmaTimePicker } from "./CellmaComponent/CellmaTimePicker";
+// export { default as CellmaUnderConstruction } from "./CellmaComponent/CellmaUnderConstruction";//custom props - isUnderConstruction, navigate, dispatch, translate, setIsUnderConstruction
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
